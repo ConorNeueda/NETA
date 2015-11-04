9381c2aa5d370f084e1503178200fd976967e5b1
Anna Presho <anna.presho@neueda.com>
2015-11-04 16:04:43 +0000

    Added constructors

--- NetaService/Utils.cs
+++ NetaService/Utils.cs
@@ -14,4 +14,50 @@ class Utils
  
         return ret;
     }
+
+    public static AverageSpeed_AveragePass_County CreateCountyAveragesFromTable(average_performance_broadband table)
+    {
+        AverageSpeed_AveragePass_County ret = new AverageSpeed_AveragePass_County();
+
+        ret.CountyID = table.county_id_fk;
+        ret.AveragePass = table.Average_Pass_of_5_GCSES;
+        ret.AverageSpeed = table.Average_Speed_mbps;
+        ret.Id = table.id;
+
+        return ret;
+    }
+
+    public static AuthorityPop_SyncSpeed CreateUptakeFromTable(la_pop_speed_ranked table)
+    {
+        AuthorityPop_SyncSpeed ret = new AuthorityPop_SyncSpeed();
+        ret.ID = table.id;
+        ret.Authority = table.authority;
+        ret.PopSize = table.pop_size;
+        ret.AverageSync = table.average_sync_speed;
+
+        return ret;
+    }
+
+    public static AuthorityEmployment_Speed CreateAuthEmploymentSpeedFromTable(authority_employment_syncspeed_rankings table)
+    {
+        AuthorityEmployment_Speed ret = new AuthorityEmployment_Speed();
+        ret.ID = table.authority_id;
+        ret.Authority = table.authority_name;
+        ret.EmploymentRate = table.employment_rate;
+        ret.SyncSpeed = table.sync_speed;
+        ret.EmploymentRate = table.employment_rate;
+        ret.SpeedRanking = table.speed_ranking;
+
+        return ret;
+    }
+
+    public static SpearmansRank CreateCorrelationsTable(spearmans_ranks table)
+    {
+        SpearmansRank ret = new SpearmansRank();
+        ret.CorrelationID = table.correlation_id;
+        ret.CorrelationName = table.correlation_name;
+        ret.SpearmansRho = table.spearmans_rho;
+
+        return ret;
+    }
 }
